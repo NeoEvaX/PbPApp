7833adfc338b188436212da8589f8b9c624ad49f
Ian Kremer <Ian.Kremer@gmail.com>
2020-10-19 22:50:32 -0700

    Clean up
    Removing API and cleaned up Project Folder.
    
    Also added some skeleton code for screens.

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      //home: MyHomePage(title: 'Flutter Demo Home Page'),
+      initialRoute: '/', //WelcomeScreen.id,
+      routes: {},
+    );
+  }
+}
